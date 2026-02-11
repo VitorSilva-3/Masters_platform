fe71cd9ca4f59e0eb4197f0167304c1a8dc9beea
VitorSilva-3 <vtsilva3@gmail.com>
2026-02-11 12:24:29 +0000

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-
-import re
-
-future_terms = ["hypothetical", "similar", "putative", "uncharacterized", "probable", "partial"]
-
-def normalize_text(s: str) -> str:
-    return re.sub(r"[^a-z0-9]", "", s.lower())
