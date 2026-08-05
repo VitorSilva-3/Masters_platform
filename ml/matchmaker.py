13a19c19af5056f4f88af9e0620fa58a44fb89d1
VitorSilva-3 <vtsilva3@gmail.com>
2026-08-05 13:53:46 +0100

    update

--- ml/matchmaker.py
+++ ml/matchmaker.py
@@ -1,7 +1,6 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
 from ml.metabolic_rules import METABOLIC_PATHWAYS, PURE_SUGARS_PATHWAYS
 
 class BiotransformationMatchmaker:
